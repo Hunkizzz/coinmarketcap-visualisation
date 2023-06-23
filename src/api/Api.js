b0c509d9941310566a6fee0658dfea9508f45700
Goniak Dmitrii <hunkizzzx777@gmail.com>
2023-06-23 20:05:09 +0300

    put all links into Api

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,9 +2,12 @@ import axios from "axios";
 
 export const Api = {
     getAllCryptoCurrencies() {
-        return axios.get('http://localhost:8095/coinmarketcap/api/crypto');
+        return axios.get('http://localhost:8095/coinmarketcap/api/history/crypto');
     },
     getSingleCryptoCurrency(name) {
-        return axios.get('http://localhost:8095/coinmarketcap/api/crypto/' + name);
+        return axios.get('http://localhost:8095/coinmarketcap/api/history/crypto/' + name);
+    },
+    getCryptoNames() {
+        return axios.get('http://localhost:8095/coinmarketcap/api/history/crypto_currency');
     }
 };
--- src/component/Layout.js
+++ src/component/Layout.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import LeftSideMenu from './LeftSideMenu'; // Update the import statement
+import { Api } from '../api/Api';
 
 function Layout({ children }) {
   const [cryptoData, setCryptoData] = useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:8095/coinmarketcap/api/current_crypto_currency')
-      .then(response => response.json())
-      .then(data => setCryptoData(data))
-      .catch(error => {
-        console.error('Error fetching crypto data:', error);
-      });
-  }, []);
+  Api.getCryptoNames()
+    .then((response) => {
+      setCryptoData(response.data)
+    })
+    .catch(error => {
+      console.error('Error fetching crypto data:', error);
+    });
 
-  return (
-      <LeftSideMenu cryptoData={cryptoData} />
-  );
+
+return (
+  <LeftSideMenu cryptoData={cryptoData} />
+);
 }
 
 export default Layout;
